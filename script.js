8b76d681f8cb0c134d79b6a4c9385a156a11e7ea
Lab05_etec <Lab05_etec@etec>
2025-06-17 17:27:41 -0300

    fazendo uns bagulho en JS

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+const pokemonNum = document.querySelector('.pokemon_num');
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonImg = document.querySelector('pokemon_img');
+
 const fetchpokemon = async (pokemon) => {
-    
     const APIresponce = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-
     const data = await APIresponce.json();
-
-    console.log(APIresponce);
+    return data;
 }
 
-fetchpokemon('25');
+const RenderPokemon = async (pokemon) => {
+    const data = await fetchpokemon(pokemon);
+    pokemonName.innerHTML = data.name;
+    pokemonNum.innerHTML = data.id;
+    pokemonImg.src = data['sprites']['version']['generation-v']['black-white']
+}
+
+RenderPokemon('25')
