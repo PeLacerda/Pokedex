eb76bbfbd723e52b5c1b6914712befae887437a8
Pedro Lacerda <pedro.lacerda.1302@gmail.com>
2025-06-27 20:43:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_num');
+const pokemonImage = document.querySelector('.pokemon_img');
+const pokemonType0 = document.querySelector('.pokemon_type_0');
+const pokemonType1 = document.querySelector('.pokemon_type_1');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+const buttonUnder = document.querySelector('.btn-under');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
+}
+
+const renderPokemon = async (pokemon) => {
+
+  pokemonName.innerHTML = 'Carregando...';
+  pokemonNumber.innerHTML = '';
+
+  const data = await fetchPokemon(pokemon);
+
+  if (data) {
+    pokemonImage.style.display = 'block';
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    input.value = '';
+    searchPokemon = data.id;
+    pokemonType0.innerHTML = data['types']['0']['type']['name'];
+    pokemonType1.innerHTML = data['types']['1']['type']['name'];
+
+  } else {
+    pokemonImage.style.display = 'none';
+    pokemonName.innerHTML = 'Not found :c';
+    pokemonNumber.innerHTML = '';
+  }
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+});
+
+buttonUnder.addEventListener('click', () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
+});
+
+buttonNext.addEventListener('click', () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
+
+
+
+renderPokemon(searchPokemon);
